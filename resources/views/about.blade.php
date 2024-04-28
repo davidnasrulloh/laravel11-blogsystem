4bf2528791e1a08734535f779af1b29f3bb48600
David Nasrulloh <davidkrb52@gmail.com>
2024-04-28 22:50:34 +0700

    chore: initial commit, setup

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+    </div>
+    <h1>Halaman about</h1>
+    <h3>Owner : {{ $nama }} {{ $umur }}</h3>
+    <img src="img/main-main.jpg" alt="" width="200">
+
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman blog</title>
+</head>
+<body>
+    <div>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+    </div>
+    <h1>Halaman blog</h1>
+    <article>
+        <h4>Judul 1</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis placeat ipsam? Molestias non vitae a fuga nam, sequi expedita?</p>
+    </article>
+
+    <article>
+        <h4>Judul 2</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis placeat ipsam? Molestias non vitae a fuga nam, sequi expedita?</p>
+    </article>
+</body>
+</html>
