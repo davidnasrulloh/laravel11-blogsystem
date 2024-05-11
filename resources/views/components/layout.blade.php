42c5e07742da180ba699ae3f1f9f7f80233fa563
David Nasrulloh <davidkrb52@gmail.com>
2024-05-11 15:44:28 +0700

    feat: view data in laravel 11

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,13 +12,13 @@
 </head>
 <body class="h-full">
     <!--
-  This example requires updating your template:
+      This example requires updating your template:
 
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
+      ```
+      <html class="h-full bg-gray-100">
+      <body class="h-full">
+      ```
+    -->
 <div class="min-h-full">
     <x-navbar></x-navbar>
     <x-header>{{ $title }}</x-header>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,9 +18,55 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', [
-        'title' => 'Blog'
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Posts',
+        'posts' => [
+            [
+                'id' => 1,
+                'title' => 'Judul Artikel 1',
+                'slug' => 'judul-artikel-1',
+                'author' => 'David Nasrulloh',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed ipsum distinctio ipsam blanditiis ex qui! Adipisci et iure quidem laudantium!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Judul Artikel 2',
+                'slug' => 'judul-artikel-2',
+                'author' => 'David Nasrulloh',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed ipsum distinctio ipsam blanditiis ex qui! Adipisci et iure quidem laudantium!'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'David Nasrulloh',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed ipsum distinctio ipsam blanditiis ex qui! Adipisci et iure quidem laudantium!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'slug' => 'judul-artikel-2',
+            'author' => 'David Nasrulloh',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed ipsum distinctio ipsam blanditiis ex qui! Adipisci et iure quidem laudantium!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    // dd($post);
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
     ]);
 });
 
